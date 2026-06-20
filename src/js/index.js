52a9687653d5abc3a213ed66fe25474a7c1e0d6a
Praneeth1136 <praneethkilaparthi@gmail.com>
2026-06-21 00:52:45 +0530

    Bypass Vercel NPM build errors by locally embedding loconative-scroll

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import LoconativeScroll from "loconative-scroll";
+import LoconativeScroll from "./loconative-scroll.js";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { copyText } from "./utils/index";
